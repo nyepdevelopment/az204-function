7e61237459690f39bf35339a3c12ae1c22c3a0fb
nyepdevelpment <nyep.development@gmail.com>
2024-07-29 22:20:00 +0800

    first commit

---
+++ HttpExample.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace My.Function
+{
+    public class HttpExample
+    {
+        private readonly ILogger<HttpExample> _logger;
+
+        public HttpExample(ILogger<HttpExample> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HttpExample")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
